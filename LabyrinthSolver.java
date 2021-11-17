94057c60072f3a67381a5c5379b512c14a78377b
Ren <sren@friendsbalt.org>
2021-11-17 12:48:24 -0500

    -

--- LabyrinthSolver.java
+++ LabyrinthSolver.java
@@ -1,12 +1,23 @@
+import java.util.*;
 public class LabyrinthSolver{
 	
 	public LabyrinthSolver(){
 	}
 	
-	public void findSafeMove(int row, int col, Labyrinth l){
+	public int[] findSafeMove(int row, int col, Labyrinth l){
+		ArrayList<Integer> solution = new ArrayList<Integer>();
+		
 		if(row == l.rows - 1 && row == l.cols - 1){
 			//show solution
 			// break out of backtracking
+			return solution.toArray();
+		}
+		for(int[] X : {l.UP, l.DOWN, l.LEFT, l.RIGHT}){
+			if(isValid(row + X[0], col + X[1], l)){
+				//makeMove(x)
+				findSafeMove(row + X[0], col + X[1], l);
+				//undo(x)
+			}
 		}
 	}
 	
